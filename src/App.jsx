5a9bef520262a4e53f4840338deccf70b3145b1c
Istiack Rohan <istiackador@gmail.com>
2024-02-06 12:31:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Componenets/StickyNavbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+      <div className="">
+        <h1>Empty</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componenets/StickyNavbar.jsx
@@ -0,0 +1,12 @@
+import './StickyNavbar.css';
+
+export default function StickyNavbar(){
+    return (
+        <>
+            <div className="container">
+                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png" alt="logo" className='logo'/>
+                
+            </div>
+        </>
+    )
+}
